407f7e82edab66aefd2d360fdc4d6d3ca28a9016
Daniel <danielcrewe1999@gmail.com>
2021-10-14 00:02:59 +0100

    Creating windows batch file for lazy install

---
+++ install.bat
@@ -0,0 +1,39 @@
+@echo off
+
+cls
+title SNIPPET INSTALL SCRIPT
+color 04
+echo ---- WARNING ----
+echo.
+echo This script will only work on windows machines,
+echo Also it MUST be placed in the same drive as your roaming folder as this is where snippets are stored.
+echo USE AT OWN RISK.
+echo.
+echo -----------------
+echo.
+echo CURRENT DRIVE LOCATION: %CD:~0,3%
+echo.
+set /p consent= "PROCEED (Y/N): "
+if /i "%consent%"=="y" goto installcommand
+if /i "%consent%"=="n"  goto exitcommand
+
+cls
+echo Input not recognised...
+goto commonexit
+
+:installcommand
+cls
+color 2
+echo Installing...
+xcopy %cd%\VSCSNIPPETS-V1.code-snippets %UserProfile%\AppData\Roaming\Code\User\snippets\test
+goto commonexit
+
+:exitcommand
+cls
+echo Install cancelled...
+goto commonexit
+
+:commonexit
+echo Console will now terminate...
+timeout 6 > NUL
+
